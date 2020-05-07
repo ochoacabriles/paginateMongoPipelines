f528840689922df589ccc06560eda1d13937a378
Rafael <ochoacabriles@gmail.com>
2020-05-07 18:07:48 -0500

    Add validation to avoid errors due to wrong data types received

--- index.js
+++ index.js
@@ -1,25 +1,29 @@
-const pipelinePagination = ({ page, pageSize, sortBy }) =>
-[
-  // Sort by _id
-  {
-    $sort: { [sortBy]: 1 }
-  },
-  // Group to get count of relevant without losing results
-  {
-    $group: {
-      _id: null,
-      count: { $sum: 1 },
-      results: { $push: '$$ROOT' }
+const pipelinePagination = ({ page = 1, pageSize = 100, sortBy = '_id', order = 1 }) => {
+  const orderToUse = order !== 1 && order !== -1 ? 1 : order;
+  const pageSizeToUse = pageSize % 1 === 0 ? pageSize : 100;
+  const pageToUse = page % 1 === 0 ? page : 1;
+  [
+    // Sort by _id
+    {
+      $sort: { [sortBy]: orderToUse }
+    },
+    // Group to get count of relevant without losing results
+    {
+      $group: {
+        _id: null,
+        count: { $sum: 1 },
+        results: { $push: '$$ROOT' }
+      }
+    },
+    // Apply skip and limit to results
+    {
+      $project: {
+        _id: 0,
+        count: 1,
+        results: { $slice: ['$results', pageSizeToUse * (pageToUse - 1), pageSizeToUse] }
+      }
     }
-  },
-  // Apply skip and limit to results
-  {
-    $project: {
-      _id: 0,
-      count: 1,
-      results: { $slice: ['$results', pageSize * (page - 1), pageSize] }
-    }
-  }
-];
+  ];
+}
 
 module.exports = pipelinePagination;
